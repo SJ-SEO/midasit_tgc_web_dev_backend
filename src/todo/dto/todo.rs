decdf38386f4957934d3e526c93d2ef63e26f2ad
ssj0324 <ssj0324@midasit.com>
2024-05-28 10:26:14 +0900

    clone from hws-origin

---
+++ src/todo/dto/todo.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+use super::request::CreateTodoRequest;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TodoDto {
+    pub id: i32,
+    pub content: String,
+    pub done: i8,
+}
+
+impl TodoDto {
+    // pub fn new(id: i32, content: String, done: i8) -> Self {
+    //     Self { id, content, done }
+    // }
+
+    pub fn new_from_request(rq: CreateTodoRequest) -> Self {
+        Self {
+            id: 0,
+            content: rq.content.clone(),
+            done: 0,
+        }
+    }
+}
